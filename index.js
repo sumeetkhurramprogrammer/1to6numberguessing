98f679592f52a8b776da655710fd65fee5ff1bb4
sumeetkhurramprogrammer <163676467+sumeetkhurramprogrammer@users.noreply.github.com>
2024-03-23 01:26:06 +0500

    my first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+#! /usr/bin/env node
+//shahbang
+const Numberrandom = Math.floor(Math.random() * 6 + 1);
+import inquirer from "inquirer";
+let answer = await inquirer.prompt([
+    {
+        name: "please guess a num 1-6",
+        type: "number",
+        messege: "please guess a num",
+    }
+]);
+if (answer.usergussednum === Numberrandom) {
+    console.log("congratulation you guessed right num");
+}
+else {
+    console.log("you gussed wrong number");
+    console.log("the number was=" + Numberrandom);
+}
